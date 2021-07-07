4e003d0123660f933a76124473ec30376e46a0e5
Brian Palay <bpplaysguitar@hotmail.com>
2021-07-07 10:34:47 -0500

    started installing packages and getting starter code from prev repo

---
+++ index.js
@@ -0,0 +1,72 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generate = require('./src/generateHTML')
+// const create = require('./src/generateEmployee');
+// let employeeData = ``;
+
+
+const initialPrompt = 
+[
+  {
+    type: 'list',
+    message: "What would you like to do?",
+    choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager'],
+    name: 'whaToDo',
+},
+]
+
+const removeEmployeePrompt = 
+[
+  {
+    type: 'list',
+    message: "Which employee do you want to remove?",
+    choices: ['TODO is this variables?'],
+    name: 'removeEmployee',
+},
+]
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(initalPrompt)
+  .then((res) => {
+    res.role = "Manager";
+    employeeData += create.employee(res);
+    if (res.conStop === 'Continue') {
+      employeeFunction();
+    } else {
+      const fileName = "./dist/index.html"
+      
+      fs.writeFile(
+        fileName,
+        generate.HTML(employeeData),
+        err => err ? console.error(err) : console.log("Team profiles successfully generated."));
+
+    }
+
+
+
+  })
+
+}
+
+      function employeeFunction() {
+        inquirer.prompt(employeePrompt).then((res) => {
+            if (res.askAgain) {
+                employeeData += create.employee(res);
+                employeeFunction();
+            } else {
+                employeeData += create.employee(res);
+                const fileName = "./dist/index.html";
+    
+                fs.writeFile(
+                    fileName,
+                    generate.HTML(employeeData),
+                    err => err ? console.error(err) : console.log("Team profiles successfully generated."));
+            }
+        })
+    };
+
+
+// Function call to initialize app
+init();
