4579febbd90490ed012d79a1060588b7c37b50aa
Brian Palay <bpplaysguitar@hotmail.com>
2021-07-08 21:38:03 -0500

    deleted data from tables

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -10,11 +10,6 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
-INSERT INTO department (name)
-VALUES  ("Management"),
-		("Race Talent");
-
-
 CREATE TABLE role (
   role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
@@ -22,12 +17,6 @@ CREATE TABLE role (
   department_id INT,
   PRIMARY KEY (role_id)
 );
-
-INSERT INTO role (title, salary, department_id)
-VALUES  ("Team Principal", 8000000.00, 1),
-		("Primary Driver", 15000000.00, 2),
-		("Reserve Driver", 1000000.00, 2);
-
 
 CREATE TABLE employee (
   employee_id INT NOT NULL AUTO_INCREMENT,
@@ -37,21 +26,3 @@ CREATE TABLE employee (
   manager_id INT NULL,
   PRIMARY KEY (employee_id)
 );
-
-INSERT INTO employee (first_name, last_name, employee_role_id, manager_id)
-VALUES 	("Christian", "Horner", 1, null),
-        ("Toto", "Wolff", 1, null),
-        ("Guenther", "Steiner", 1, null),
-		("Daniel", "Ricciardo", 2, 1),
-		("Max", "Verstappen", 2, 1),
-        ("Sebastian", "Vettel", 2, 2),
-        ("Valtteri", "Bottas", 2, 2),
-        ("Charles", "Leclerc", 2, 3),
-        ("Carlos", "Sainz", 2, 3),
-        ("Pierre", "Gasly", 3, 1),
-        ("Lando", "Norris", 3, 2),
-        ("Nico", "Hulkenberg", 3, 3);
-
-SELECT * from department;
-SELECT * from role;
-SELECT * from employee;
